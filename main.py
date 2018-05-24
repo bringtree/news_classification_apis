98a48b6cbe9484aa5b79a9965beba938353a39ce
peisong <bringtree@qq.com>
2018-05-24 17:21:22 +0800

    添加了代码注释

--- main.py
+++ main.py
@@ -6,24 +6,20 @@ import joblib
 import numpy as np
 import jieba
 from tensorflow.python.framework import graph_util
+import configparser
+
+
 # export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
 
-batch_size = 64
-save_file_num = 10
-word_vec_size = 400
-word_vec_len = 50
-base_acc = 0.96
-base_f1_score = 0.85
-save_pb = True
-pb_file = './server/test.pb'
-iter_num = 50
-model_src = "./save_model/"
-
-word_vec = joblib.load("./data/word_dict_encoder.pkl")
-
-
 def make_batches(x_src, y_src, batch_size):
+    """
+    生成 batches
+    :param x_src: 输入的新闻标题的numpy路径
+    :param y_src:  输入的标签的numpy路径
+    :param batch_size: batch_size 设置的大小
+    :return: 生成的新闻标题的batches,生成标签的batches以及batches的数量
+    """
     x = np.load(x_src)
     y = np.load(y_src)
 
@@ -54,11 +50,21 @@ def make_batches(x_src, y_src, batch_size):
     return data_x_batches, data_y_batches, batches_num
 
 
-def encoder_data(batch_num, x_batches, y_batches, word_vec, word_vec_len, word_vec_size):
+def encoder_data(batches_num, x_batches, y_batches, word_vec, word_vec_len, word_vec_size):
+    """
+    对输入的新闻标题进行编码，和数据格式整理的扩充
+    :param batches_num: batches_num的数目
+    :param x_batches: 未编码前 新闻标题 的batches
+    :param y_batches: 未格式化前 标签 的batches
+    :param word_vec: 用来编码的字典
+    :param word_vec_len: 新闻标题的最大长度
+    :param word_vec_size: 编码的词向量的字典的大小
+    :return:
+    """
     data_x = []
     data_y = []
     data_len = []
-    for i in range(batch_num):
+    for i in range(batches_num):
         x = x_batches[i]
         y = y_batches[i]
         data_len.append([len(sentence) for sentence in x])
@@ -78,13 +84,28 @@ def encoder_data(batch_num, x_batches, y_batches, word_vec, word_vec_len, word_v
 
 
 if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('./params.cfg')
+    batch_size = config.get('hyperparameters', 'batch_size')
+    save_file_num = config.get('hyperparameters', 'save_file_num')
+    word_vec_size = config.get('hyperparameters', 'word_vec_size')
+    word_vec_len = config.get('hyperparameters', 'word_vec_len')
+    iter_num = config.get('hyperparameters', 'iter_num')
+    base_acc = config.get('plugin', 'base_acc')
+    base_f1_score = config.get('plugin', 'base_f1_score')
+    save_pb_mode = config.get('plugin', 'save_pb_mode')
+    pb_file = config.get('data', 'pb_filepath')
+    model_src = config.get('data', 'model_filepath')
+    word_vec_path = config.get('data', 'word_vec_filepath')
+    word_vec = joblib.load(word_vec_path)
 
-    train_x_batches, train_y_batches, train_batch_num = make_batches("0_train_X.npy", "0_train_Y.npy", batch_size)
-    train_x, train_y, train_len = encoder_data(train_batch_num, train_x_batches, train_y_batches, word_vec,
+    # 数据预处理
+    train_x_batches, train_y_batches, train_batches_num = make_batches("0_train_X.npy", "0_train_Y.npy", batch_size)
+    train_x, train_y, train_len = encoder_data(train_batches_num, train_x_batches, train_y_batches, word_vec,
                                                word_vec_len,
                                                word_vec_size)
-    test_x_batches, test_y_batches, test_batch_num = make_batches("0_test_X.npy", "0_test_Y.npy", batch_size)
-    test_x, test_y, test_len = encoder_data(test_batch_num, test_x_batches, test_y_batches, word_vec, word_vec_len,
+    test_x_batches, test_y_batches, test_batches_num = make_batches("0_test_X.npy", "0_test_Y.npy", batch_size)
+    test_x, test_y, test_len = encoder_data(test_batches_num, test_x_batches, test_y_batches, word_vec, word_vec_len,
                                             word_vec_size)
     ###############################################################
 
@@ -93,13 +114,15 @@ if __name__ == '__main__':
     lstm_model = model.Model()
     lstm_model.build()
     sess.run(tf.global_variables_initializer())
+
+    # 模型加载和保存
     saver = tf.train.Saver(max_to_keep=save_file_num)
     ckpt = tf.train.get_checkpoint_state(model_src)
 
     if ckpt and ckpt.model_checkpoint_path:
         saver.restore(sess, ckpt.model_checkpoint_path)
 
-    if save_pb is True:
+    if save_pb_mode is True:
         constant_graph = tf.graph_util.convert_variables_to_constants(
             sess,
             sess.graph_def,
@@ -112,14 +135,14 @@ if __name__ == '__main__':
 
     for iter in range(iter_num):
         loss, right_num, err_num, TP, FP, TN, FN, = 0, 0, 0, 0, 0, 0, 0
-        for i in range(train_batch_num):
+        for i in range(train_batches_num):
             loss += lstm_model.train(
                 sess,
                 input_sentences=train_x[i],
                 input_labels=train_y[i],
                 input_length=train_len[i]
             )
-        for i in range(test_batch_num):
+        for i in range(test_batches_num):
             result = lstm_model.predict(
                 sess,
                 input_sentences=test_x[i],
@@ -145,6 +168,7 @@ if __name__ == '__main__':
         acc = right_num / (err_num + right_num)
         f1_score = 2 * precision * recall / (precision + recall + 1e-5)
 
+        # 保存模型
         if (acc > base_acc) and (f1_score > base_f1_score):
             if not os.path.exists(model_src):
                 os.makedirs(model_src)
--- model.py
+++ model.py
@@ -4,8 +4,22 @@ import focal_loss
 
 
 class Model():
-    def __init__(self, sentence_len=None, learning_rate=None, wordVec_size=None, hidden_num=None, output_keep_prob=None,
+    """
+    lstm 模型
+    """
+
+    def __init__(self, sentence_len=None, learning_rate=None, word_vec_size=None, hidden_num=None,
+                 output_keep_prob=None,
                  batch_size=None):
+        """
+
+        :param sentence_len: 句子长度
+        :param learning_rate: 学习速率
+        :param wordVec_size: 词向量的大小
+        :param hidden_num: lstm 隐藏层 size 大小
+        :param output_keep_prob: Dropout 层 output 损失 (0,1]
+        :param batch_size: batch_size 的大小
+        """
         if sentence_len:
             self.sentence_len = sentence_len
         else:
@@ -14,10 +28,10 @@ class Model():
             self.learning_rate = learning_rate
         else:
             self.learning_rate = 0.001
-        if wordVec_size:
-            self.wordVec_size = wordVec_size
+        if word_vec_size:
+            self.word_vec_size = word_vec_size
         else:
-            self.wordVec_size = 400
+            self.word_vec_size = 400
         if hidden_num:
             self.hidden_num = hidden_num
         else:
@@ -27,8 +41,11 @@ class Model():
         else:
             self.output_keep_prob = 0.5
 
-
     def build(self):
+        """
+        构建 lstm 模型
+        :return: Model对象
+        """
         self.input_sentences = tf.placeholder(shape=[None, self.sentence_len, self.wordVec_size],
                                               name='input_sentences',
                                               dtype=tf.float32)
@@ -37,49 +54,55 @@ class Model():
         sentence_input = tf.transpose(self.input_sentences, [1, 0, 2])
 
         self.input_length = tf.placeholder(shape=[None], name='input_length', dtype=tf.int32)
-        forward_lstm = tf.contrib.rnn.LSTMCell(self.hidden_num, initializer=tf.orthogonal_initializer())
-        backward_lstm = tf.contrib.rnn.LSTMCell(self.hidden_num, initializer=tf.orthogonal_initializer())
-        forward_drop = tf.contrib.rnn.DropoutWrapper(forward_lstm, output_keep_prob=self.output_keep_prob)
-        backward_drop = tf.contrib.rnn.DropoutWrapper(backward_lstm, output_keep_prob=self.output_keep_prob)
 
-        # output_fw will be a `Tensor` shaped:
-        #           `[max_time, batch_size, cell_fw.output_size]`
-        #           and output_bw will be a `Tensor` shaped:
-        #           `[max_time, batch_size, cell_bw.output_size]`.
-        # If time_major == True, this must be a tensor of shape:
-        #         `[max_time, batch_size, ...]`, or a nested tuple of such elements.
+        with tf.variable_scope("bid_lstm_layer"):
+            forward_lstm = tf.contrib.rnn.LSTMCell(self.hidden_num, initializer=tf.orthogonal_initializer())
+            backward_lstm = tf.contrib.rnn.LSTMCell(self.hidden_num, initializer=tf.orthogonal_initializer())
+            forward_drop = tf.contrib.rnn.DropoutWrapper(forward_lstm, output_keep_prob=self.output_keep_prob)
+            backward_drop = tf.contrib.rnn.DropoutWrapper(backward_lstm, output_keep_prob=self.output_keep_prob)
 
-        encoder_outputs, encoder_final_state = tf.nn.bidirectional_dynamic_rnn(
-            cell_fw=forward_drop,
-            cell_bw=backward_drop,
-            inputs=sentence_input,
-            sequence_length=self.input_length,
-            dtype=tf.float32,
-            time_major=True)
+            encoder_outputs, encoder_final_state = tf.nn.bidirectional_dynamic_rnn(
+                cell_fw=forward_drop,
+                cell_bw=backward_drop,
+                inputs=sentence_input,
+                sequence_length=self.input_length,
+                dtype=tf.float32,
+                time_major=True)
+            lstm_output = tf.concat([encoder_final_state[0].h, encoder_final_state[1].h], 1)
 
-        lstm_output = tf.concat([encoder_final_state[0].h, encoder_final_state[1].h], 1)
-        intent_W = tf.get_variable(
-            initializer=tf.random_uniform([self.hidden_num * 2, 2], -0.1, 0.1),
-            dtype=tf.float32, name="intent_W")
-        intent_b = tf.get_variable(initializer=tf.zeros([2]), dtype=tf.float32, name="intent_b")
-        predict_intent = tf.matmul(lstm_output, intent_W) + intent_b
-        # cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.one_hot(self.input_labels, 2),
-        #                                                         logits=predict_intent)
-        cross_entropy = focal_loss.focal_loss(
-            prediction_tensor=predict_intent, target_tensor=tf.one_hot(self.input_labels, 2), weights=None, alpha=0.25,
-            gamma=2
-        )
-        self.loss = tf.reduce_mean(cross_entropy)
-        self.output_intent = tf.argmax(predict_intent, 1,name='output_result')
+        with tf.variable_scope('full_connection_layer'):
+            intent_w = tf.get_variable(
+                initializer=tf.random_uniform([self.hidden_num * 2, 2], -0.1, 0.1),
+                dtype=tf.float32, name="intent_w")
+            intent_b = tf.get_variable(initializer=tf.zeros([2]), dtype=tf.float32, name="intent_b")
+            predict_intent = tf.matmul(lstm_output, intent_w) + intent_b
+
+        with tf.variable_scope('predict_result'):
+            self.output_intent = tf.argmax(predict_intent, 1, name='output_result')
+
+        with tf.variable_scope("loss_function"):
+            cross_entropy = focal_loss.focal_loss(
+                prediction_tensor=predict_intent, target_tensor=tf.one_hot(self.input_labels, 2), weights=None,
+                alpha=0.25,
+                gamma=2
+            )
+            self.loss = tf.reduce_mean(cross_entropy)
 
         with tf.variable_scope("optimizer_function"):
             optimizer = tf.train.AdamOptimizer(name="a_optimizer", learning_rate=self.learning_rate)
             self.grads, self.vars = zip(*optimizer.compute_gradients(self.loss))
-            print("vars for loss function: ", self.vars)
             self.gradients, _ = tf.clip_by_global_norm(self.grads, 5)  # clip gradients
             self.train_op = optimizer.apply_gradients(zip(self.gradients, self.vars))
 
     def train(self, sess, input_sentences, input_labels, input_length):
+        """
+
+        :param sess: tf.Session() 返回的对象
+        :param input_sentences: 训练的句子 shape = [batch_size, self.sentence_len, self.wordVec_size],
+        :param input_labels: 训练的标签 shape = [batch_size]
+        :param input_length: 训练的句子长度 shape = [batch_size]
+        :return: loss大小 int
+        """
         loss, _ = sess.run([self.loss, self.train_op], feed_dict={
             self.input_sentences: input_sentences,
             self.input_labels: input_labels,
@@ -88,25 +111,32 @@ class Model():
         return loss
 
     def predict(self, sess, input_sentences, input_length):
+        """
+
+        :param sess: tf.Session() 返回的对象
+        :param input_sentences: 预测的句子 shape = [batch_size, self.sentence_len, self.wordVec_size],
+        :param input_length: 预测的标签 shape = [batch_size]
+        :return: 预测的结果 shape = [batch_size]
+        """
         result = sess.run([self.output_intent], feed_dict={
             self.input_sentences: input_sentences,
             self.input_length: input_length
         })
         return result
-
+# 测试
 # if __name__ == '__main__':
-    # sess = tf.Session()
-    # lstm_model = Model(sentence_len=50, learning_rate=1, wordVec_size=2, hidden_num=100, output_keep_prob=0.5,
-    #                    batch_size=3)
-    # lstm_model.build()
-    # sess.run(tf.global_variables_initializer())
-    # s = lstm_model.train(
-    #     sess,
-    #     input_sentences=[[[v*0.1,1] for v in range(50)],
-    #                      [[v*0.2,1] for v in range(50)],
-    #                      [[v*0.3,1] for v in range(50)]
-    #                      ],
-    #     input_labels=[0, 1, 0],
-    #     input_length=[3, 2, 1]
-    # )
-    # print(s)
+#     sess = tf.Session()
+#     lstm_model = Model(sentence_len=50, learning_rate=1, wordVec_size=2, hidden_num=100, output_keep_prob=0.5,
+#                        batch_size=3)
+#     lstm_model.build()
+#     sess.run(tf.global_variables_initializer())
+#     s = lstm_model.train(
+#         sess,
+#         input_sentences=[[[v * 0.1, 1] for v in range(50)],
+#                          [[v * 0.2, 1] for v in range(50)],
+#                          [[v * 0.3, 1] for v in range(50)]
+#                          ],
+#         input_labels=[0, 1, 0],
+#         input_length=[3, 2, 1]
+#     )
+#     print(s)
